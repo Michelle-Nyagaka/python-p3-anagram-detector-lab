69055fff2836147903b332db43cf594a3c4da9f1
Michelle Nyagaka <michellenyagaka@gmail.com>
2025-11-24 22:38:23 +0300

    sorted

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()      # normalize to lowercase
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        matches = []
+
+        for word in words:
+            # avoid matching the same exact word
+            if word.lower() == self.word:
+                continue
+
+            # compare sorted letters
+            if sorted(word.lower()) == self.sorted_word:
+                matches.append(word)
+
+        return matches
+
